2e64cae69f945422d2ff64716e73b31142b798dc
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-08-11 14:50:08 -0400

    commit tree 493fde537110d58e7d635dff7255f111d8f430cf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function isWin(game){
+    return game.result === "W"
+}
+
+function superbowlWin(record){
+    let win = record.find(isWin)
+    if(win !== undefined){
+        return win.year
+    }
+    return undefined
+
+}
